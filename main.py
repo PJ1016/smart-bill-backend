44414595a5ec516c85f6cd45d638201028689b86
PJ1016 <41801479+PJ1016@users.noreply.github.com>
2025-12-03 15:15:45 +0530

    Add CORS support

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app=FastAPI(title="My First FASTAPI")
+origins = [
+    "http://localhost:5173",  # local React dev
+    "https://blue-pond-0e0901c10.3.azurestaticapps.net",  # deployed React app
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/hello/{name}")
 def say_hello(name:str):
